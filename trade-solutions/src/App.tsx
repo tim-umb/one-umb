2d77e65c525f0533f98bda582142204f312b8da5
tim-umb <129091750+tim-umb@users.noreply.github.com>
2024-02-05 14:38:04 -0700

    Imported from Quest

---
+++ trade-solutions/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import questTheme from 'src/QuestUiKitLightTheme';
+import MainPage from 'src/components/MainPage/MainPage';
+
+function App() {
+  return (
+    <HelmetProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={questTheme}>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+          </Routes>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </HelmetProvider>
+  );
+}
+
+export default App;
