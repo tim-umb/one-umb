623df99420ffbeb78c3d4da26615fe693fa09123
Timmy <tim_watts@ymail.com>
2024-02-05 22:06:18 -0700

    updates

--- trade-solutions/src/components/MainPage/MainPage.tsx
+++ trade-solutions/src/components/MainPage/MainPage.tsx
@@ -13,7 +13,7 @@
  **********************************************************************/
 
 import React from 'react';
-import { Typography, Button, SvgIcon } from '@mui/material';
+import { Typography, Button } from '@mui/material';
 import VectorImage from 'src/assets/images/mainPage_Vector.png';
 import Vector1Image from 'src/assets/images/mainPage_Vector_1.png';
 import Vector2Image from 'src/assets/images/mainPage_Vector_2.png';
