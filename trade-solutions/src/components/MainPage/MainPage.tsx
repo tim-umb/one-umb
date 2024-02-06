57363914c14046f33454a9648147cbd7b40ba50c
Timmy <tim_watts@ymail.com>
2024-02-05 18:20:46 -0700

    update

--- trade-solutions/src/components/MainPage/MainPage.tsx
+++ trade-solutions/src/components/MainPage/MainPage.tsx
@@ -1275,7 +1275,7 @@ const Frame16: any = styled('div')(({ theme }: any) => ({
   padding: `0px`,
   boxSizing: `border-box`,
   alignSelf: `stretch`,
-  height: `448px`,
+  height: `fit-content`,
   margin: '0px auto',
   maxWidth: '1440px',
   width: '100%',
